caeaa7accc1a81723ae82133207ea54bc5d8f09c
putera-ilham <puterailham93@gmail.com>
2025-11-04 17:00:30 +0800

    Add files via upload

---
+++ type_hints.py
@@ -0,0 +1,14 @@
+# age: int
+# name: str
+# height: float
+# is_human: bool
+
+### --- TYPE HINTS --- ###
+def police_check(age: int) -> bool:
+    if age > 18:
+        can_drive = True
+    else:
+        can_drive = False
+    return can_drive
+
+print(police_check(19))
